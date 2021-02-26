ccdbb7b53d7abac6817a4a0ca8f6a4d4d28a535a
Maciej Koch <maciej.k00h@gmail.com>
2021-02-26 01:33:47 +0100

    fixed reading from file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { spawn } = require("child_process");
+const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-let data = require("./data.json");
+let _data;
 const minutes = 0.5;
 
 const id = setInterval(() => {
@@ -12,12 +13,18 @@ const id = setInterval(() => {
   console.log(" i should have exec script");
   python.on("close", (code) => {
     console.log(`Child process close all stdio with code ${code}`);
-    data = require("./data.json");
+    fs.readFile("./data.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      _data = JSON.parse(data);
+    });
   });
 }, minutes * 60 * 1000);
 
 app.get("/", (req, res) => {
-  res.json(data);
+  res.json(_data);
 });
 
 app.listen(port, () => console.log(`Works!`));
